5e90ad96323db2112f6af661fc0c9e680f6bd707
Boris <borik1and@gmail.com>
2023-09-05 14:14:15 +0300

    added def test_mask_account_number

--- tests/pytest.py
+++ tests/pytest.py
@@ -1,5 +1,7 @@
-from utils.functions import open_json_file_r
+from utils.functions import mask_account_number
 from utils.functions import mask_card_number
+from utils.functions import open_json_file_r
+
 
 def test_json_file():
     # Укажите путь к вашему JSON-файлу
@@ -13,5 +15,10 @@ def test_json_file():
     actual_data = open_json_file_r(file_path)
     assert actual_data == expected_data
 
+
 def test_mask_card_number():
     assert mask_card_number('1596837868705199') == 'XXXX XX** **** 5199'
+
+
+def test_mask_account_number():
+    assert mask_account_number('1596837868705199') == '**5199'
