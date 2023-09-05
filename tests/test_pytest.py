700300f97c397c0a84a709e2ddc65ee0864020f7
Boris <borik1and@gmail.com>
2023-09-05 16:30:57 +0300

    changes in test_pytest.py def test_json_file

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,3 +1,5 @@
+import os
+
 from utils.functions import mask_account_number
 from utils.functions import mask_card_number
 from utils.functions import open_json_file_r
@@ -5,14 +7,13 @@ from utils.functions import open_json_file_r
 
 def test_json_file():
     # Укажите путь к вашему JSON-файлу
-    file_path = 'file_test.json'
+    file_test = os.path.expanduser('~/account_transactions/tests/file_test.json')
     # Ожидаемые данные
     expected_data = [{
         "key1": "value1",
         "key2": "value2",
-
     }]
-    actual_data = open_json_file_r(file_path)
+    actual_data = open_json_file_r(file_test)
     assert actual_data == expected_data
 
 
