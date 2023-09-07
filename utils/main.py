dd4fd35cfcacc892f1c73a1b3737976bab1f022d
Boris <borik1and@gmail.com>
2023-09-07 19:02:46 +0300

    changing main code

--- utils/main.py
+++ utils/main.py
@@ -1,3 +1,10 @@
-from utils.functions import process_operations
+import os
 
-process_operations()
+from utils.functions import process_operations, open_json_file_r
+
+database_user = os.path.expanduser('~/account_transactions/operations.json')
+operations_data = open_json_file_r(database_user)
+
+formatted_operations = process_operations(operations_data)
+for i, operation in enumerate(formatted_operations, start=1):
+    print(operation)
