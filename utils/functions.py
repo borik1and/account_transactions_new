06ae469fdd71c4735275fa121783eef487fb5ac0
Boris <borik1and@gmail.com>
2023-09-07 16:44:22 +0300

    change code from main.py to def process_operations()

--- utils/functions.py
+++ utils/functions.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def open_json_file_r(database):
@@ -18,9 +19,32 @@ def mask_account_number(account_number):
     """Функция для маскирования номера счета"""
     return f'**{account_number[-4:]}'
 
+
 def convert_date(converting_date):
     """функция преобразования даты из американского стандарта в стандарт ДД.ММ.ГГГГ"""
     date_parts = converting_date.split('-')
     formatted_date = '-'.join(date_parts[::-1])
     return formatted_date
 
+
+def process_operations():
+    database_user = os.path.expanduser('~/account_transactions/operations.json')
+    operations_data = open_json_file_r(database_user)
+
+    # Фильтрация и сортировка операций
+    executed_operations = [operation for operation in operations_data if operation.get('state') == 'EXECUTED']
+    sorted_operations = sorted(executed_operations, key=lambda x: x['date'], reverse=True)
+
+    # Выбор последних 5 операций
+    latest_operations = sorted_operations[:5]
+
+    for operation in latest_operations:
+        date = operation['date'].split('T')[0]
+        description = operation['description']
+        from_account = mask_card_number(operation.get('from', ''))
+        to_account = mask_account_number(operation['to'])
+        amount = operation['operationAmount']['amount']
+        currency = operation['operationAmount']['currency']['name']
+        formatted_operation = (f"{convert_date(date)} {description}\n"
+                               f"{from_account} -> {to_account}\n{amount} {currency}\n")
+        print(formatted_operation)
--- utils/main.py
+++ utils/main.py
@@ -1,28 +1,33 @@
-import os
-from utils.functions import mask_account_number
-from utils.functions import mask_card_number
-from utils.functions import open_json_file_r
-from utils.functions import convert_date
+# import os
+# from utils.functions import mask_account_number, mask_card_number, open_json_file_r, convert_date
+#
+# def process_operations():
+#     database_user = os.path.expanduser('~/account_transactions/operations.json')
+#     operations_data = open_json_file_r(database_user)
+#
+#     # Фильтрация и сортировка операций
+#     executed_operations = [operation for operation in operations_data if operation.get('state') == 'EXECUTED']
+#     sorted_operations = sorted(executed_operations, key=lambda x: x['date'], reverse=True)
+#
+#     # Выбор последних 5 операций
+#     latest_operations = sorted_operations[:5]
+#
+#     formatted_operations = []
+#     for operation in latest_operations:
+#         date = operation['date'].split('T')[0]
+#         description = operation['description']
+#         from_account = mask_card_number(operation.get('from', ''))
+#         to_account = mask_account_number(operation['to'])
+#         amount = operation['operationAmount']['amount']
+#         currency = operation['operationAmount']['currency']['name']
+#
+#         formatted_operation = f"{convert_date(date)} {description}\n{from_account} -> {to_account}\n{amount} {currency}\n"
+#
+#         formatted_operations.append(formatted_operation)
+#
+#     return formatted_operations
 
-database_user = os.path.expanduser('~/account_transactions/operations.json')
-operations_data = open_json_file_r(database_user)
 
-# Фильтрация и сортировка операций
-executed_operations = [operation for operation in operations_data if operation.get('state') == 'EXECUTED']
-sorted_operations = sorted(executed_operations, key=lambda x: x['date'], reverse=True)
+from utils.functions import process_operations
 
-# Выбор последних 5 операций
-latest_operations = sorted_operations[:5]
-
-# Форматирование и вывод операций
-for operation in latest_operations:
-    date = operation['date'].split('T')[0]
-    description = operation['description']
-    from_account = mask_card_number(operation.get('from', ''))
-    to_account = mask_account_number(operation['to'])
-    amount = operation['operationAmount']['amount']
-    currency = operation['operationAmount']['currency']['name']
-
-    formatted_operation = f"{convert_date(date)} {description}\n{from_account} -> {to_account}\n{amount} {currency}\n"
-
-    print(formatted_operation)
+process_operations()
