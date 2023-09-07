430360fda4039f229b035d71726daadde0c50f39
Boris <borik1and@gmail.com>
2023-09-07 19:01:31 +0300

    add def test_process_operations()

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,9 +1,11 @@
 import os
 
-from utils.functions import convert_date
-from utils.functions import mask_account_number
-from utils.functions import mask_card_number
-from utils.functions import open_json_file_r
+from utils.functions import convert_date, mask_account_number, mask_card_number, open_json_file_r, process_operations
+
+
+# from utils.functions import mask_account_number
+# from utils.functions import mask_card_number
+# from utils.functions import open_json_file_r
 
 
 def test_json_file():
@@ -29,3 +31,29 @@ def test_mask_account_number():
 def test_convert_date():
     date_amer = "2010-10-02"
     assert convert_date(date_amer) == '02-10-2010'
+
+
+def test_process_operations():
+    operations_data = [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+
+    ]
+
+    formatted_operations = process_operations(operations_data)
+
+    """Проверяем ожидаемый результат"""
+    assert len(formatted_operations) == 1
